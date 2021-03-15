8c97baad77d04b27b284b7bcf7a59e74dc83503f
Emerson D <frenow@outlook.com>
2021-03-15 13:32:03 -0300

    initial commit

---
+++ src/components/Content.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+import '../styles/content.scss';
+import { MovieCard } from './MovieCard';
+
+export function Content({ movies, selectedGenre}) {
+
+
+  return (
+  <div className="container">
+  <header>
+    <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+  </header>
+
+  <main>
+    <div className="movies-list">
+      {movies.map(movie => (
+        <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+      ))}
+    </div>
+  </main>
+</div>
+  )
+}
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,26 @@
+import '../styles/sidebar.scss';
+import { Button } from './Button';
+
+
+export function SideBar({ genres, selectedGenreId, handleClickButton }) {
+
+
+  return (
+  <nav className="sidebar">
+  <span>Watch<p>Me</p></span>
+
+  <div className="buttons-container">
+    {genres.map(genre => (
+      <Button
+        id={String(genre.id)}
+        title={genre.title}
+        iconName={genre.name}
+        onClick={() => handleClickButton(genre.id)}
+        selected={selectedGenreId === genre.id}
+      />
+    ))}
+  </div>
+
+</nav>
+  )
+}
